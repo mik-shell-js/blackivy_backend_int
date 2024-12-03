6f4fbb2724106659e101dd81600d3f5090a01b62
Michel <mha57@cornell.edu>
2024-12-03 18:01:05 -0500

    fixed yarn build issues

--- src/app/[...page]/page.tsx
+++ src/app/[...page]/page.tsx
@@ -1,26 +1,17 @@
-// Example file structure, app/[...page]/page.tsx
-// You could alternatively use src/app/[...page]/page.tsx
 import { builder } from "@builder.io/sdk";
 import { RenderBuilderContent } from "../components/builder";
-import React from "react";
+import { GetStaticPropsContext } from "next";
 
-
-// Replace with your Public API Key
+// Initialize Builder.io
 builder.init("706c4001d29248a197cd4cb1e707e1f2");
-
-export async function generateStaticParams() {
-  return []; // Add any pre-rendered routes if necessary
-}
 
 export default async function Page({
   params,
-}: {
-  params: { page?: string | string[] };
-}) {
+}: GetStaticPropsContext<{ page: string[] }>) {
   const model = "page";
 
   const urlPath =
-    "/" + (Array.isArray(params.page) ? params.page.join("/") : params.page || "");
+    "/" + (Array.isArray(params?.page) ? params.page.join("/") : params?.page || "");
 
   const content = await builder
     .get(model, {
@@ -30,10 +21,9 @@ export default async function Page({
     })
     .toPromise();
 
-  return (
-    <>
-      {/* Render the Builder page */}
-      <RenderBuilderContent content={content} model={model} />
-    </>
-  );
+  if (!content) {
+    return <div>Page Not Found</div>;
+  }
+
+  return <RenderBuilderContent content={content} model={model} />;
 }
