b3212faebf585c1168d2116db7d0ae76c21f9b4d
Michel <mha57@cornell.edu>
2024-12-03 17:35:30 -0500

    removed page props interface

--- src/app/[...page]/page.tsx
+++ src/app/[...page]/page.tsx
@@ -8,30 +8,24 @@ import React from "react";
 // Replace with your Public API Key
 builder.init("706c4001d29248a197cd4cb1e707e1f2");
 
-interface PageProps {
-  params: {
-    page: string | string[]; // Allow a single string or an array of strings
-  };
-}
-
-export default async function Page({params}: PageProps) {
+// Define your async page function
+export default async function Page({
+  params,
+}: {
+  params: { page?: string | string[] };
+}) {
   const model = "page";
 
-  // Check if `params.page` is an array or a string
+  // Resolve URL path dynamically
   const urlPath =
-  "/" + (Array.isArray(params.page) ? params.page.join("/") : params.page || "");
+    "/" + (Array.isArray(params.page) ? params.page.join("/") : params.page || "");
 
   const content = await builder
-    // Get the page content from Builder with the specified options
     .get(model, {
       userAttributes: {
-        // Use the page path specified in the URL to fetch the content
         urlPath,
       },
-      // Set prerender to false to return JSON instead of HTML
-      prerender: false,
     })
-    // Convert the result to a promise
     .toPromise();
 
   return (
